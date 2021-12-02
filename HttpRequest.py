896eb63046543fdef009dbe9dadce6858fd5a794
Jack Hermanson <jack.hermanson@live.com>
2021-12-01 22:23:56 -0700

    pass in headers directly

--- HttpRequest.py
+++ HttpRequest.py
@@ -10,18 +10,18 @@ import sys
 class HttpRequest:
     reading: Reading
 
-    headers = {
-        "Authentication": os.getenv(API_KEY)
-    }
-
     def post(self):
+        api_key = os.getenv(API_KEY)
         if not self.reading:
             raise TypeError("Reading cannot be None")
 
         response = requests.post("https://cs370-temp-reader.herokuapp.com/api/measurements",
-                                 headers=self.headers,
+                                 headers={
+                                     "Authentication": api_key
+                                 },
                                  data=self.reading.__dict__())
         error = response.status_code != 200
         if error:
+            print(api_key)
             print(response.request.headers)
             print(response.request.body)
